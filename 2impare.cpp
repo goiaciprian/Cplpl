edab142186f37e12f44468e7505e3e92f2828f6b
vsDeus <44678820+vsDeus@users.noreply.github.com>
2019-07-09 09:12:29 +0300

    Update 2impare.cpp

--- 2impare.cpp
+++ 2impare.cpp
@@ -8,10 +8,8 @@ int main(void)
     int n;
     cin >> n;
 
-    if (n % 2 == 0)
-        cout << n - 3 << " " << n - 1;
-    else
-        cout << n - 4 << " " << n - 2;
+    if (n % 2 == 0) cout << n - 3 << " " << n - 1;
+    else cout << n - 4 << " " << n - 2;
 
     return 0;
 }
