d35e1e5a59cbdfcc1691b7cb368043b7680d2ce9
vsDeus <camioane112@gmail.com>
2019-10-05 13:56:50 +0300

    update 5.X.2019

---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main(void) {
+
+    int test {};
+
+    std::cin >> test;
+
+    printf ("test");
+    printf ("test");
+    printf ("test");
+    printf ("test");
+    printf ("test");
+    printf ("test");
+    printf ("test");
+    printf ("test");
+    printf ("test");
+    printf ("test");
+    printf("\n");
+
+    std::cout << test;
+    
+    return 0;
+
+}
